0fc8e95460a5674fd96a07e254bbe9f20ee00d10
edokeh <edokeh@163.com>
2013-07-09 09:38:19 +0800

    fix error

--- js/seajs-config.js
+++ js/seajs-config.js
@@ -1,24 +1,3 @@
-/*map start*/
-seajs.production = true;
-if(seajs.production){
-    seajs.config({
-        map : [
-	[
-		"base/handlebars.js",
-		"base/handlebars-c876a0d8f3bf3ac4bb5697bfd90eaa80.js"
-	],
-	[
-		"blogs/new.js",
-		"blogs/new-279fcebdcda1b4f1bd2d88a90e7dc80c.js"
-	],
-	[
-		"users/index.js",
-		"users/index-dba9ddc268d9f63694cb918f835df8bc.js"
-	]
-]
-    });
-}
-/*map end*/
 seajs.config({
     alias : {
         '$' : 'gallery/jquery/1.9.1/jquery',
